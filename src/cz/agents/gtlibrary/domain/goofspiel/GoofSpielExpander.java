0cb0d509150fe4ef11d2ad4001997d459d3b1cf7
jcermak <jcermak@localhost>
2013-04-09 14:20:05 +0200

    Goofspiel can now use fixed nature sequence or all possible nature cards

--- src/cz/agents/gtlibrary/domain/goofspiel/GoofSpielExpander.java
+++ src/cz/agents/gtlibrary/domain/goofspiel/GoofSpielExpander.java
@@ -11,6 +11,8 @@ import cz.agents.gtlibrary.interfaces.InformationSet;
 
 public class GoofSpielExpander<I extends InformationSet> extends ExpanderImpl<I> {
 
+	private static final long serialVersionUID = -2513008286051108758L;
+
 	public GoofSpielExpander(AlgorithmConfig<I> algConfig) {
 		super(algConfig);
 	}
@@ -21,15 +23,23 @@ public class GoofSpielExpander<I extends InformationSet> extends ExpanderImpl<I>
 		List<Action> actions = new LinkedList<Action>();
 
 		if(gsState.isPlayerToMoveNature()) {
-			actions.add(gsState.getNatureSequence().getFirst());
+			if(GSGameInfo.useFixedNatureSequence) {
+				actions.add(gsState.getNatureSequence().getFirst());
+			} else {
+				addCardsForPlayerToMove(gsState, actions);
+			}
 			return actions;
 		}
+		addCardsForPlayerToMove(gsState, actions);
+//		Collections.shuffle(actions, new Random(GSGameInfo.seed));
+		return actions;
+	}
+
+	public void addCardsForPlayerToMove(GoofSpielGameState gsState, List<Action> actions) {
 		for (Integer actionValue : gsState.getCardsForPlayerToMove()) {
-			GoofSpielAction action = new GoofSpielAction(actionValue, gsState.getPlayerToMove(), getAlgorithmConfig().getInformationSetFor(gameState));
+			GoofSpielAction action = new GoofSpielAction(actionValue, gsState.getPlayerToMove(), getAlgorithmConfig().getInformationSetFor(gsState));
 			
 			actions.add(action);
 		}
-//		Collections.shuffle(actions, new Random(GSGameInfo.seed));
-		return actions;
 	}
 }
