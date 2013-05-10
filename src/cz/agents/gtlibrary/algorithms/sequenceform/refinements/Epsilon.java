86e0e492655c7ee9a221fbc58c20946e579d2e5f
jcermak <jcermak@localhost>
2013-05-10 15:26:39 +0200

    Strategy sanity check added, adjustable constrains and lower bounds added to LPTable

--- src/cz/agents/gtlibrary/algorithms/sequenceform/refinements/Epsilon.java
+++ src/cz/agents/gtlibrary/algorithms/sequenceform/refinements/Epsilon.java
@@ -3,43 +3,40 @@ package cz.agents.gtlibrary.algorithms.sequenceform.refinements;
 public class Epsilon extends Number {
 
 	private static final long serialVersionUID = 525903728831840335L;
-	
+
 	private double value;
-	
+
 	public Epsilon() {
 		value = -1;
 	}
-	
+
 	public void setValue(double value) {
 		this.value = value;
 	}
-	
+
 	@Override
 	public int intValue() {
-		if (value == -1) 
-			throw new UnsupportedOperationException("Epsilon not yet initialized");
-		return (int) value;
+		return (int) doubleValue();
 	}
 
 	@Override
 	public long longValue() {
-		if (value == -1) 
-			throw new UnsupportedOperationException("Epsilon not yet initialized");
-		return (long) value;
+		return (long) doubleValue();
 	}
 
 	@Override
 	public float floatValue() {
-		if (value == -1) 
-			throw new UnsupportedOperationException("Epsilon not yet initialized");
-		return (float) value;
+		return (float) doubleValue();
 	}
 
 	@Override
 	public double doubleValue() {
-		if (value == -1) 
-			throw new UnsupportedOperationException("Epsilon not yet initialized");
 		return value;
+	}
+
+	@Override
+	public String toString() {
+		return value == -1 ? "eps" : Double.toString(doubleValue());
 	}
 
 }
