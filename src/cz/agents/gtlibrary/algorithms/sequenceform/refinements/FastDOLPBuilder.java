b29fd2175638babf09baf62d5af06bbd06077a02
Jiri Cermak <jiri.cermak@agents.fel.cvut.cz>
2013-07-03 15:28:42 +0200

    DO LP builders fixed

--- src/cz/agents/gtlibrary/algorithms/sequenceform/refinements/FastDOLPBuilder.java
+++ src/cz/agents/gtlibrary/algorithms/sequenceform/refinements/FastDOLPBuilder.java
@@ -64,8 +64,6 @@ public class FastDOLPBuilder extends DOLPBuilder {
 
 	public void updateUtility(DoubleOracleConfig<DoubleOracleInformationSet> config, Sequence p1Sequence, Sequence p2Sequence) {
 		Double utility = config.getUtilityForSequences(p1Sequence, p2Sequence);
-//		Key eqKey = new Key(p1Sequence);
-//		Key varKey = new Key(p2Sequence);
 
 		lpTable.clearConstraint(p1Sequence, p2Sequence);
 		if (utility != null)
