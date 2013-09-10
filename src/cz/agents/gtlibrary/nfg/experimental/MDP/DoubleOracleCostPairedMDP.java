5493e798a90055b863cdf20ba95e0be01108c8f3
Branislav Bosansky <branislav.bosansky@agents.fel.cvut.cz>
2013-09-10 10:59:02 +0200

    CostPairMDP v0.361 - logging added

--- src/cz/agents/gtlibrary/nfg/experimental/MDP/DoubleOracleCostPairedMDP.java
+++ src/cz/agents/gtlibrary/nfg/experimental/MDP/DoubleOracleCostPairedMDP.java
@@ -155,5 +155,15 @@ public class DoubleOracleCostPairedMDP {
 
         long endTime = System.nanoTime() - startTime;
         debugOutput.println("Overall Time: " + (endTime / 1000000));
+        debugOutput.println("final size: FirstPlayer Marginal Strategies: " + firstPlayerStrategy.getAllMarginalsInStrategy().size() + " \t SecondPlayer Marginal Strategies: " + secondPlayerStrategy.getAllMarginalsInStrategy().size());
+        debugOutput.println("final result:" + UB);
+
+        try {
+            Runtime.getRuntime().gc();
+            Thread.sleep(500l);
+        } catch (InterruptedException e) {
+        }
+
+        System.out.println("final memory:" + ((Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()) / 1024 / 1024));
     }
 }
