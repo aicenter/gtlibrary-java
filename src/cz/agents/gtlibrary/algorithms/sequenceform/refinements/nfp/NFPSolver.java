0dd278a69785abb78e039da6dfba8fdca94e651d
Jiri Cermak <jiri.cermak@agents.fel.cvut.cz>
2013-10-18 16:19:04 +0200

    NFP solver now works for both players

--- src/cz/agents/gtlibrary/algorithms/sequenceform/refinements/nfp/NFPSolver.java
+++ src/cz/agents/gtlibrary/algorithms/sequenceform/refinements/nfp/NFPSolver.java
@@ -4,6 +4,8 @@ import java.util.Map;
 
 import cz.agents.gtlibrary.algorithms.sequenceform.SequenceFormConfig;
 import cz.agents.gtlibrary.algorithms.sequenceform.SequenceInformationSet;
+import cz.agents.gtlibrary.domain.aceofspades.AoSExpander;
+import cz.agents.gtlibrary.domain.aceofspades.AoSGameState;
 import cz.agents.gtlibrary.domain.upordown.UDExpander;
 import cz.agents.gtlibrary.domain.upordown.UDGameState;
 import cz.agents.gtlibrary.interfaces.Expander;
@@ -16,9 +18,22 @@ public class NFPSolver {
 	private Expander<SequenceInformationSet> expander;
 	
 	public static void main(String[] args) {
+//		runUpOrDown();
+		runAceOfSpades();
+	}
+
+	private static void runUpOrDown() {
 		NFPSolver solver = new NFPSolver(new UDGameState(), new UDExpander<SequenceInformationSet>(new SequenceFormConfig<SequenceInformationSet>()));
 		
-		System.out.println(solver.solve());
+		System.out.println(solver.solveForP1());
+		System.out.println(solver.solveForP2());
+	}
+	
+	private static void runAceOfSpades() {
+		NFPSolver solver = new NFPSolver(new AoSGameState(), new AoSExpander<SequenceInformationSet>(new SequenceFormConfig<SequenceInformationSet>()));
+		
+		System.out.println(solver.solveForP1());
+		System.out.println(solver.solveForP2());
 	}
 	
 	public NFPSolver(GameState root, Expander<SequenceInformationSet> expander) {
@@ -26,7 +41,7 @@ public class NFPSolver {
 		this.expander = expander;
 	}
 	
-	public Map<Sequence, Double> solve() {
+	public Map<Sequence, Double> solveForP1() {
 		InitialPBuilder initPbuilder = new InitialPBuilder(expander, root);
 		
 		initPbuilder.buildLP();
@@ -37,6 +52,8 @@ public class NFPSolver {
 		initQBuilder.buildLP();
 		IterationData data = initQBuilder.solve();
 		
+		if(data.getLastItSeq().isEmpty())
+			return data.getRealizationPlan();
 		while(Math.abs(data.getGameValue()) > 1e-8) {
 			PBuilder pBuilder = new PBuilder(expander, root, data, initialValue);
 			
@@ -47,6 +64,38 @@ public class NFPSolver {
 			
 			qBuilder.buildLP();
 			data = qBuilder.solve();
+			if(data.getLastItSeq().isEmpty())
+				return data.getRealizationPlan();
+		}
+		return data.getRealizationPlan();
+	}
+	
+	public Map<Sequence, Double> solveForP2() {
+		InitialP2PBuilder initPbuilder = new InitialP2PBuilder(expander, root);
+		
+		initPbuilder.buildLP();
+		double initialValue = initPbuilder.solve();
+		
+		InitialP2QBuilder initQBuilder = new InitialP2QBuilder(expander, root, initialValue);
+		
+		initQBuilder.buildLP();
+		IterationData data = initQBuilder.solve();
+		
+		if(data.getLastItSeq().isEmpty())
+			return data.getRealizationPlan();
+		while(Math.abs(data.getGameValue()) > 1e-8) {
+			P2PBuilder pBuilder = new P2PBuilder(expander, root, data, initialValue);
+			
+			pBuilder.buildLP();
+			double currentValue = pBuilder.solve();
+			
+			P2QBuilder qBuilder = new P2QBuilder(expander, root, initialValue, currentValue, data);
+			
+			qBuilder.buildLP();
+			data = qBuilder.solve();
+			
+			if(data.getLastItSeq().isEmpty())
+				return data.getRealizationPlan();
 		}
 		return data.getRealizationPlan();
 	}
---
+++ src/cz/agents/gtlibrary/algorithms/sequenceform/refinements/nfp/P2QBuilder.java
@@ -0,0 +1,67 @@
+package cz.agents.gtlibrary.algorithms.sequenceform.refinements.nfp;
+
+import ilog.concert.IloException;
+import ilog.cplex.IloCplex.UnknownObjectException;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+import cz.agents.gtlibrary.algorithms.sequenceform.SequenceInformationSet;
+import cz.agents.gtlibrary.algorithms.sequenceform.refinements.LPData;
+import cz.agents.gtlibrary.interfaces.Expander;
+import cz.agents.gtlibrary.interfaces.GameState;
+import cz.agents.gtlibrary.interfaces.Sequence;
+
+public class P2QBuilder extends InitialP2QBuilder {
+
+	private Map<Sequence, Double> explSeqSum;
+
+	public P2QBuilder(Expander<SequenceInformationSet> expander, GameState rootState, double initialValue, double gameValue, IterationData data) {
+		super(expander, rootState, initialValue);
+		this.explSeqSum = getSum(data.getLastItSeq(), data.getExplSeqSum(), gameValue);
+		lpFileName = "P2qLP.lp";
+	}
+
+	@Override
+	public void updateLPForFirstPlayer(GameState state) {
+		super.updateLPForFirstPlayer(state);
+		Object p1Sequence = state.getSequenceForPlayerToMove();
+		Double value = explSeqSum.get(p1Sequence);
+
+		if (value != null)
+			lpTable.setConstraint(p1Sequence, "s", explSeqSum.get(p1Sequence));
+	}
+
+	@Override
+	protected void updateP1Parent(GameState state) {
+		super.updateP1Parent(state);
+		Sequence p1Sequence = state.getSequenceFor(players[0]);
+
+		if (p1Sequence.size() == 0)
+			return;
+		Double value = explSeqSum.get(p1Sequence);
+
+		if (value != null)
+			lpTable.setConstraint(p1Sequence, "s", explSeqSum.get(p1Sequence));
+	}
+
+	@Override
+	protected Map<Sequence, Double> getSum(Set<Sequence> exploitableSequences, Map<Sequence, Double> explSeqSum, double valueOfGame) {
+		Map<Sequence, Double> updatedSum = new HashMap<Sequence, Double>(explSeqSum);
+
+		for (Sequence sequence : exploitableSequences) {
+			updatedSum.put(sequence, updatedSum.get(sequence) + valueOfGame);
+		}
+		return updatedSum;
+	}
+	
+	@Override
+	protected IterationData createIterationData(LPData lpData) throws UnknownObjectException, IloException {
+		Map<Sequence, Double> watchedSequenceValues = getWatchedUSequenceValues(lpData);
+		Set<Sequence> exploitableSequences = getExploitableSequences(watchedSequenceValues);
+		Map<Sequence, Double> realizationPlan = getRealizationPlan(lpData);
+		
+		return new IterationData(lpData.getSolver().getObjValue(), explSeqSum, exploitableSequences, realizationPlan);
+	}
+}
