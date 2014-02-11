7a031c4bdbc9aa11c60238dd9d233180863610ff
bosansky <bosansky@localhost>
2014-02-11 16:52:18 +0100

    refactoring: stochastic games moved to experimental package

---
+++ src/cz/agents/gtlibrary/experimental/stochastic/experiment/ExperimentInfo.java
@@ -0,0 +1,51 @@
+package cz.agents.gtlibrary.experimental.stochastic.experiment;
+
+import cz.agents.gtlibrary.iinodes.PlayerImpl;
+import cz.agents.gtlibrary.interfaces.GameInfo;
+import cz.agents.gtlibrary.interfaces.Player;
+
+public class ExperimentInfo implements GameInfo {
+
+	public static double UTILITY = 1;
+	public static Player PATROLLER = new PlayerImpl(0);
+	public static Player ATTACKER = new PlayerImpl(1);
+	public static Player NATURE = new PlayerImpl(2);
+	public static Player[] ALL_PLAYERS = new Player[] { PATROLLER, ATTACKER, NATURE };
+	public static String patrollerStartId = "0";
+	public static String graphFile = "exp_small.txt";
+	public static double epsilon = 0.05;
+	public static int commitmentDepth = 2;
+
+	@Override
+	public double getMaxUtility() {
+		return UTILITY;
+	}
+
+	@Override
+	public Player getFirstPlayerToMove() {
+		return PATROLLER;
+	}
+
+	@Override
+	public Player getOpponent(Player player) {
+		return player.equals(ATTACKER) ? PATROLLER : ATTACKER;
+	}
+
+	@Override
+	public String getInfo() {
+		// TODO Auto-generated method stub
+		return null;
+	}  
+
+	@Override
+	public int getMaxDepth() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Player[] getAllPlayers() {
+		return ALL_PLAYERS;
+	}
+
+}
---
+++ src/cz/agents/gtlibrary/experimental/stochastic/newexperiment/NatureAction.java
@@ -0,0 +1,62 @@
+package cz.agents.gtlibrary.experimental.stochastic.newexperiment;
+
+import cz.agents.gtlibrary.experimental.stochastic.StochasticAction;
+import cz.agents.gtlibrary.interfaces.GameState;
+import cz.agents.gtlibrary.interfaces.Player;
+
+public class NatureAction extends StochasticAction {
+
+	private static final long serialVersionUID = -1336366637039514482L;
+	private NEGameState state;
+	private Player player;
+
+	public NatureAction(NEGameState state, Player player) {
+		this.state = state;
+		this.player = player;
+	}
+
+	public NEGameState getGameState() {
+		return state;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((player == null) ? 0 : player.hashCode());
+		result = prime * result + ((state == null) ? 0 : state.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		NatureAction other = (NatureAction) obj;
+		if (player == null) {
+			if (other.player != null)
+				return false;
+		} else if (!player.equals(other.player))
+			return false;
+		if (state == null) {
+			if (other.state != null)
+				return false;
+		} else if (!state.equals(other.state))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "NA" + state;
+	}
+
+	@Override
+	public void perform(GameState gameState) {
+		throw new UnsupportedOperationException();
+	}
+}
