dcfbab7648f820ea7f998e439ba19098ab918602
jiricermak <jiricermak@jiricermak-PC>
2014-06-05 16:14:43 +0200

    Added standalone refinement solvers and refinement solvers prepared for use in DOuble oracle algorithm, added Matching pennies on Christmas morning domain

---
+++ src/cz/agents/gtlibrary/algorithms/sequenceform/doubleoracle/nfplp/RecyclingNFPTable.java
@@ -0,0 +1,8 @@
+package cz.agents.gtlibrary.algorithms.sequenceform.doubleoracle.nfplp;
+
+import cz.agents.gtlibrary.algorithms.sequenceform.refinements.RecyclingLPTable;
+
+public class RecyclingNFPTable extends RecyclingLPTable {
+	
+
+}
