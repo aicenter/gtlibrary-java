c50338e3d73c586275f4b9d4bd65d4e89d2f2de9
Viliam Lisy <viliam.lisy@agents.fel.cvut.cz>
2014-07-01 14:06:08 +0200

    UCT determinism off by default

--- src/cz/agents/gtlibrary/algorithms/mcts/selectstrat/UCTSelector.java
+++ src/cz/agents/gtlibrary/algorithms/mcts/selectstrat/UCTSelector.java
@@ -17,7 +17,7 @@ import java.util.List;
  * @author vilo
  */
 public class UCTSelector implements Selector, AlgorithmData, NbSamplesProvider, ActionFrequencyProvider, MeanStrategyProvider {
-    public static boolean useDeterministicUCT = true;
+    public static boolean useDeterministicUCT = false;
 
     private UCTBackPropFactory fact;
     private List<Action> actions;
