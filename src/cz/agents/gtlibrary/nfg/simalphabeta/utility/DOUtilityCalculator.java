6c60ada56dab957b3b0c77a4dc88a50edc439833
bosansky <bosansky@localhost>
2014-07-04 11:24:27 +0200

    temporary return to the previous version until 100% verified

--- src/cz/agents/gtlibrary/nfg/simalphabeta/utility/DOUtilityCalculator.java
+++ src/cz/agents/gtlibrary/nfg/simalphabeta/utility/DOUtilityCalculator.java
@@ -157,13 +157,13 @@ public class DOUtilityCalculator implements UtilityCalculator {
     public double getUtility(GameState state, ActionPureStrategy s1, ActionPureStrategy s2) {
         Double utility = cache.getUtilityFor(s1, s2);
 
-        if (utility != null)
-            return utility;
-        if (state.isPlayerToMoveNature())
-            return computeUtilityForNature(state, s1, s2, Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY);
-        return computeUtilityOf(state, s1, s2, Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY);
-//        assert utility != null;
-//        return utility == null ? Double.NaN : utility;
+//        if (utility != null)
+//            return utility;
+//        if (state.isPlayerToMoveNature())
+//            return computeUtilityForNature(state, s1, s2, Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY);
+//        return computeUtilityOf(state, s1, s2, Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY);
+////        assert utility != null;
+        return utility == null ? Double.NaN : utility;
     }
 
     @Override
