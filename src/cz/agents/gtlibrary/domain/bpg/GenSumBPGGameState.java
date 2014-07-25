1ad43c60f6524bfbbf38b5dbe4b8ac673a593b35
jiricermak <jiricermak@jiricermak-PC>
2014-07-25 12:56:30 +0200

    GenSumBPG utility now properly returns general sum utilities

--- src/cz/agents/gtlibrary/domain/bpg/GenSumBPGGameState.java
+++ src/cz/agents/gtlibrary/domain/bpg/GenSumBPGGameState.java
@@ -9,22 +9,22 @@ public class GenSumBPGGameState extends BPGGameState {
         super(new GenSumBorderPatrollingGraph(BPGGameInfo.graphFile));
     }
 
-//    @Override
-//    public double[] getUtilities() {
-//        if (getPlayerToMove().equals(BPGGameInfo.ATTACKER)) {
-//            PatrollerAction patrollerAction = (PatrollerAction) history.getSequenceOf(BPGGameInfo.DEFENDER).getLast();
-//            AttackerAction attackerAction = (AttackerAction) history.getSequenceOf(BPGGameInfo.ATTACKER).getLast();
-//
-//            if (isCaught(patrollerAction, attackerAction))
-//                return new double[] { -ATTACKER_WINS, ATTACKER_WINS };
-//        }
-//        if (attackerPosition.equals(graph.getDestination())) {
-//            double utilityChange = ((GenSumBorderPatrollingGraph)graph).getEvaderUtilityChange(getPreviousAttackerNode());
-//
-//            return new double[]{ATTACKER_WINS + utilityChange, -ATTACKER_WINS};
-//        }
-//        return new double[] { OPEN_POSITION, OPEN_POSITION };
-//    }
+    @Override
+    public double[] getUtilities() {
+        if (getPlayerToMove().equals(BPGGameInfo.ATTACKER)) {
+            PatrollerAction patrollerAction = (PatrollerAction) history.getSequenceOf(BPGGameInfo.DEFENDER).getLast();
+            AttackerAction attackerAction = (AttackerAction) history.getSequenceOf(BPGGameInfo.ATTACKER).getLast();
+
+            if (isCaught(patrollerAction, attackerAction))
+                return new double[] { -ATTACKER_WINS, ATTACKER_WINS };
+        }
+        if (attackerPosition.equals(graph.getDestination())) {
+            double utilityChange = ((GenSumBorderPatrollingGraph)graph).getEvaderUtilityChange(getPreviousAttackerNode());
+
+            return new double[]{ATTACKER_WINS + utilityChange, -ATTACKER_WINS};
+        }
+        return new double[] { OPEN_POSITION, OPEN_POSITION };
+    }
 
     private Node getPreviousAttackerNode() {
         return ((AttackerAction)getSequenceFor(BPGGameInfo.ATTACKER).getLast()).getFromNode();
