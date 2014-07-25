b35d60db2312ad03076f6b28d95106e3aed7c5c2
Jiri Cermak <jiri.cermak@agents.fel.cvut.cz>
2014-07-25 14:16:40 +0200

    StackelbergConfig stack is now initialized properly

--- src/cz/agents/gtlibrary/domain/bpg/BPGGameInfo.java
+++ src/cz/agents/gtlibrary/domain/bpg/BPGGameInfo.java
@@ -30,7 +30,7 @@ public class BPGGameInfo implements GameInfo {
 
 	public static Player[] ALL_PLAYERS = { ATTACKER, DEFENDER };
 
-	public static int DEPTH = 6;
+	public static int DEPTH = 4;
 	public static boolean SLOW_MOVES = true;
 	public static String graphFile = "GridW3-almost-connected.txt";//"GridW4.txt";"GridW3-small.txt"
 
