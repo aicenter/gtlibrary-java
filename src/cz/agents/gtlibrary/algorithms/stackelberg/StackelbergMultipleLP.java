2c7ea207c3df03bf71588797144e10cba26f217f
Branislav Bosansky <branislav.bosansky@agents.fel.cvut.cz>
2014-07-25 15:36:33 +0200

    minor update in Incremental

--- src/cz/agents/gtlibrary/algorithms/stackelberg/StackelbergMultipleLP.java
+++ src/cz/agents/gtlibrary/algorithms/stackelberg/StackelbergMultipleLP.java
@@ -152,9 +152,9 @@ public class StackelbergMultipleLP {
 
         long startCPLEX = threadBean.getCurrentThreadCpuTime();
 //        StackelbergSequenceFormLP sequenceFormLP = new StackelbergSequenceFormLP(actingPlayers, gameConfig, expander);
-        StackelbergSequenceFormMILP sequenceFormLP = new StackelbergSequenceFormMILP(actingPlayers, gameConfig, expander);
+//        StackelbergSequenceFormMILP sequenceFormLP = new StackelbergSequenceFormMILP(actingPlayers, gameConfig, expander);
+        StackelbergSequenceFormMILPIncremental sequenceFormLP = new StackelbergSequenceFormMILPIncremental(actingPlayers, gameConfig, expander);
 //        StackelbergSequenceFormMILPOracle sequenceFormLP = new StackelbergSequenceFormMILPOracle(actingPlayers, gameConfig, expander);
-//        StackelbergSequenceFormMILPIncremental sequenceFormLP = new StackelbergSequenceFormMILPIncremental(actingPlayers, gameConfig, expander);
 
 //        Iterator i = algConfig.getIterator(rootState.getAllPlayers()[0], expander);
 //        while (i.hasNext()) {
