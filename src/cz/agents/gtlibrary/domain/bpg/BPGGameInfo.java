b28d1b857020d0dc8bc3d4a433d6048ab04ae2c1
jiricermak <jiricermak@jiricermak-PC>
2014-08-01 10:32:13 +0200

    Evader is penalized for every action with exception of waiting

--- src/cz/agents/gtlibrary/domain/bpg/BPGGameInfo.java
+++ src/cz/agents/gtlibrary/domain/bpg/BPGGameInfo.java
@@ -30,9 +30,10 @@ public class BPGGameInfo implements GameInfo {
 
 	public static Player[] ALL_PLAYERS = { ATTACKER, DEFENDER };
 
-	public static int DEPTH = 1;
-	public static boolean SLOW_MOVES = false;
+	public static int DEPTH = 4;
+	public static boolean SLOW_MOVES = true;
 	public static String graphFile = "GridW3-almost-connected.txt";//"GridW4.txt";"GridW3-small.txt"
+    public static double EVADER_MOVE_COST = 0.5/(2*DEPTH);
 
 	@Override
 	public double getMaxUtility() {
--- src/cz/agents/gtlibrary/domain/bpg/GenSumBPGGameState.java
+++ src/cz/agents/gtlibrary/domain/bpg/GenSumBPGGameState.java
@@ -1,12 +1,30 @@
 package cz.agents.gtlibrary.domain.bpg;
 
+import cz.agents.gtlibrary.algorithms.stackelberg.GeneralSumBestResponse;
 import cz.agents.gtlibrary.domain.bpg.data.GenSumBorderPatrollingGraph;
+import cz.agents.gtlibrary.interfaces.GameState;
 import cz.agents.gtlibrary.utils.graph.Node;
 
 public class GenSumBPGGameState extends BPGGameState {
 
+    private double evaderPenalty;
+
     public GenSumBPGGameState() {
         super(new GenSumBorderPatrollingGraph(BPGGameInfo.graphFile));
+        evaderPenalty = 0;
+    }
+
+    public GenSumBPGGameState(GenSumBPGGameState state) {
+        super(state);
+        evaderPenalty = state.evaderPenalty;
+    }
+
+    public void executeAttackerAction(AttackerAction action) {
+        super.executeAttackerAction(action);
+        if(action.getType().equals(AttackerAction.AttackerMovementType.SLOW))
+            evaderPenalty += BPGGameInfo.EVADER_MOVE_COST;
+        else if(action.getType().equals(AttackerAction.AttackerMovementType.QUICK))
+            evaderPenalty += 2*BPGGameInfo.EVADER_MOVE_COST;
     }
 
     @Override
@@ -16,17 +34,22 @@ public class GenSumBPGGameState extends BPGGameState {
             AttackerAction attackerAction = (AttackerAction) history.getSequenceOf(BPGGameInfo.ATTACKER).getLast();
 
             if (isCaught(patrollerAction, attackerAction))
-                return new double[] { -ATTACKER_WINS, ATTACKER_WINS };
+                return new double[]{-ATTACKER_WINS - evaderPenalty, ATTACKER_WINS};
         }
         if (attackerPosition.equals(graph.getDestination())) {
-            double utilityChange = ((GenSumBorderPatrollingGraph)graph).getEvaderUtilityChange(getPreviousAttackerNode());
+            double utilityChange = ((GenSumBorderPatrollingGraph) graph).getEvaderUtilityChange(getPreviousAttackerNode());
 
-            return new double[]{ATTACKER_WINS + utilityChange, -ATTACKER_WINS};
+            return new double[]{ATTACKER_WINS + utilityChange - evaderPenalty, -ATTACKER_WINS};
         }
-        return new double[] { OPEN_POSITION, OPEN_POSITION };
+        return new double[]{OPEN_POSITION, OPEN_POSITION};
     }
 
     private Node getPreviousAttackerNode() {
-        return ((AttackerAction)getSequenceFor(BPGGameInfo.ATTACKER).getLast()).getFromNode();
+        return ((AttackerAction) getSequenceFor(BPGGameInfo.ATTACKER).getLast()).getFromNode();
+    }
+
+    @Override
+    public GameState copy() {
+        return new GenSumBPGGameState(this);
     }
 }
