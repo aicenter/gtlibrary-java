dd9184a6a33ec80131c9b3b53674c85b67bf8249
vilo <vilo@atg03.felk.cvut.cz>
2014-10-27 18:15:49 +0100

    - Selectors and SMSelectors refactored for simpler chaining of their properties in experiments - Bug fixed in computing statistics for the conjecture

--- src/cz/agents/gtlibrary/algorithms/mcts/experiments/SMConvergenceExperiment.java
+++ src/cz/agents/gtlibrary/algorithms/mcts/experiments/SMConvergenceExperiment.java
@@ -34,6 +34,8 @@ import cz.agents.gtlibrary.algorithms.mcts.distribution.StrategyCollector;
 import cz.agents.gtlibrary.algorithms.mcts.nodes.InnerNode;
 import cz.agents.gtlibrary.algorithms.mcts.nodes.Node;
 import cz.agents.gtlibrary.algorithms.mcts.oos.OOSAlgorithmData;
+import cz.agents.gtlibrary.algorithms.mcts.selectstrat.ConjectureFactory;
+import cz.agents.gtlibrary.algorithms.mcts.selectstrat.ABackPropFactory;
 import cz.agents.gtlibrary.algorithms.mcts.selectstrat.Exp3BackPropFactory;
 import cz.agents.gtlibrary.algorithms.mcts.selectstrat.RMBackPropFactory;
 import cz.agents.gtlibrary.algorithms.mcts.selectstrat.UCTBackPropFactory;
@@ -62,6 +64,8 @@ import cz.agents.gtlibrary.utils.io.GambitEFG;
 import cz.agents.gtlibrary.strategy.Strategy;
 import java.io.FileOutputStream;
 import java.io.PrintStream;
+import java.lang.management.ManagementFactory;
+import java.lang.management.ThreadMXBean;
 import java.util.ArrayDeque;
 import java.util.Arrays;
 import java.util.Date;
@@ -119,7 +123,8 @@ public class SMConvergenceExperiment {
     
     
     public static void setupGoofSpiel(int d){
-        assert GSGameInfo.depth==d;
+        GSGameInfo.depth = d;
+        GSGameInfo.regenerateCards = true;
         gameInfo = new GSGameInfo();
         rootState = new GoofSpielGameState();
         expander = new GoofSpielExpander<MCTSInformationSet>(new MCTSConfig());
@@ -210,18 +215,18 @@ public class SMConvergenceExperiment {
         }
         System.out.println();
     }
-    public static void runMCTSExp3() throws Exception {
-        assert algorithm.equals("Exp3");
+    public static void runISMCTS() throws Exception {
+        assert algorithm.equals("Exp3") || !keepExploration;
         
         Distribution dist = new MeanStratDist();
 
         ISMCTSAlgorithm alg = new ISMCTSAlgorithm(
                     rootState.getAllPlayers()[0],
                     new DefaultSimulator(expander),
-                    new Exp3BackPropFactory(-1, 1, gamma, keepExploration),
-                    //new RMBackPropFactory(-1, 1, gamma),
+                    algorithm.equals("Exp3") ? (propagateMeans ? new ABackPropFactory(new Exp3BackPropFactory(-1, 1, gamma, keepExploration)) : new ConjectureFactory(new Exp3BackPropFactory(-1, 1, gamma, keepExploration)))
+                            : (propagateMeans ? new ABackPropFactory(new RMBackPropFactory(-1, 1, gamma)) : new ConjectureFactory(new RMBackPropFactory(-1, 1, gamma))),
                     rootState, expander);
-        alg.returnMeanValue=propagateMeans;
+        //alg.returnMeanValue=propagateMeans;  //means should not be really propagated like this
 
         assert !buildCompleteTree;
         
@@ -233,23 +238,46 @@ public class SMConvergenceExperiment {
 
         Strategy strategy0 = null;
         Strategy strategy1 = null;
-        String outLine = "";
-        System.out.print("P1BRs: ");
+        StringBuilder p0brs = new StringBuilder();
+        StringBuilder p1brs = new StringBuilder();
+        StringBuilder times = new StringBuilder();
+        
+        
+        ThreadMXBean threadBean = ManagementFactory.getThreadMXBean();
+        int its = 1000;
+        int totalIts = 0;
+        double totalTime=0;
+        
 
-        for (int i = 0; i < 100; i++) {
-            alg.runIterations(iterations);
+        for (; totalIts < iterations;) {
+            long start = threadBean.getCurrentThreadCpuTime();
+            alg.runIterations(its);
+            totalTime += (threadBean.getCurrentThreadCpuTime() - start)/1e6;
+            totalIts+=its;
+            
             strategy0 = StrategyCollector.getStrategyFor(alg.getRootNode(), rootState.getAllPlayers()[0], dist);
             strategy1 = StrategyCollector.getStrategyFor(alg.getRootNode(), rootState.getAllPlayers()[1], dist);
 
-            System.out.print(brAlg1.calculateBR(rootState, ISMCTSExploitability.filterLow(strategy0)) + " ");
-            System.out.flush();
-            outLine += brAlg0.calculateBR(rootState, ISMCTSExploitability.filterLow(strategy1)) + " ";
+            p1brs.append(brAlg1.calculateBR(rootState, ISMCTSExploitability.filterLow(strategy0)) + " ");
+            p0brs.append(brAlg0.calculateBR(rootState, ISMCTSExploitability.filterLow(strategy1)) + " ");
+            times.append(totalTime + " ");
+            if (!propagateMeans){
+                if (alg.getRootNode().getGameState().isPlayerToMoveNature())
+                    System.out.println(((InnerNode)alg.getRootNode().getChildren().values().iterator().next()).getInformationSet().getAlgorithmData().toString());
+                else 
+                    System.out.println(alg.getRootNode().getInformationSet().getAlgorithmData().toString());
+                System.out.flush();
+            }
+            
 
             //System.out.println("Strat: " + strategy0.fancyToString(rootState, expander, rootState.getAllPlayers()[0]));
             //System.out.println("BR: " + brAlg.getFullBRSequences());
+            its *= 1.2;
         }
         System.out.println();
-        System.out.println("P0BRs: " + outLine);
+        System.out.println("P0BRs: " + p0brs.toString());
+        System.out.println("P1BRs: " + p1brs.toString());
+        System.out.println("Times: " + times.toString());
         //System.out.println("Strat: " + strategy0.fancyToString(rootState, expander, rootState.getAllPlayers()[0]));
         //System.out.println("Strat: " + strategy1.fancyToString(rootState, expander, rootState.getAllPlayers()[1]));
     }
@@ -290,14 +318,18 @@ public class SMConvergenceExperiment {
         //System.out.println("Strat: " + strategy0.fancyToString(rootState, expander, rootState.getAllPlayers()[0]));
         //System.out.println("Strat: " + strategy1.fancyToString(rootState, expander, rootState.getAllPlayers()[1]));
     }
-    
-        public static void runSMMCTS_Exp3() throws Exception {        
+   
+    public static void runSMMCTS_Decoupled() throws Exception {        
         Distribution dist = new MeanStratDist();
 
+        assert algorithm.equals("Exp3") || !keepExploration;
+        
         SMMCTSAlgorithm alg = new SMMCTSAlgorithm(
                     rootState.getAllPlayers()[0],
                     new DefaultSimulator(expander),
-                    new SMConjectureFactory(gamma),
+                    //new SMConjectureFactory(gamma),
+                    algorithm.equals("Exp3") ? new SMConjectureFactory(new Exp3BackPropFactory(-1, 1, gamma, keepExploration), propagateMeans) 
+                            : new SMConjectureFactory(new RMBackPropFactory(-1, 1, gamma), propagateMeans),
                     rootState, expander);
 
         assert !buildCompleteTree;
@@ -307,24 +339,44 @@ public class SMConvergenceExperiment {
 
         Strategy strategy0 = null;
         Strategy strategy1 = null;
-        String outLine = "";
-        System.out.print("P1BRs: ");
+        StringBuilder p0brs = new StringBuilder();
+        StringBuilder p1brs = new StringBuilder();
+        StringBuilder times = new StringBuilder();
+        
+        
+        ThreadMXBean threadBean = ManagementFactory.getThreadMXBean();
+        int its = 1000;
+        int totalIts = 0;
+        double totalTime=0;
+        
 
-        for (int i = 0; i < 10000; i++) {
-            alg.runIterations(iterations);
+        for (; totalIts < iterations;) {
+            long start = threadBean.getCurrentThreadCpuTime();
+            alg.runIterations(its);
+            totalTime += (threadBean.getCurrentThreadCpuTime() - start)/1e6;
+            totalIts+=its;
+            
             strategy0 = StrategyCollector.getStrategyFor(alg.getRootNode(), rootState.getAllPlayers()[0], dist);
             strategy1 = StrategyCollector.getStrategyFor(alg.getRootNode(), rootState.getAllPlayers()[1], dist);
 
-            System.out.println(brAlg1.calculateBR(rootState, ISMCTSExploitability.filterLow(strategy0)) + " ");
-            System.out.println(alg.getRootNode().getInformationSet().getAlgorithmData());
+            p1brs.append(brAlg1.calculateBR(rootState, ISMCTSExploitability.filterLow(strategy0)) + " ");
+            p0brs.append(brAlg0.calculateBR(rootState, ISMCTSExploitability.filterLow(strategy1)) + " ");
+            times.append(totalTime + " ");
+            if (alg.getRootNode().getGameState().isPlayerToMoveNature())
+                System.out.println(((InnerNode)alg.getRootNode().getChildren().values().iterator().next()).getInformationSet().getAlgorithmData().toString());
+            else 
+                System.out.println(alg.getRootNode().getInformationSet().getAlgorithmData().toString());
             System.out.flush();
-            outLine += brAlg0.calculateBR(rootState, ISMCTSExploitability.filterLow(strategy1)) + " ";
+            
 
             //System.out.println("Strat: " + strategy0.fancyToString(rootState, expander, rootState.getAllPlayers()[0]));
             //System.out.println("BR: " + brAlg.getFullBRSequences());
+            its *= 1.2;
         }
         System.out.println();
-        System.out.println("P0BRs: " + outLine);
+        System.out.println("P0BRs: " + p0brs.toString());
+        System.out.println("P1BRs: " + p1brs.toString());
+        System.out.println("Times: " + times.toString());
         //System.out.println("Strat: " + strategy0.fancyToString(rootState, expander, rootState.getAllPlayers()[0]));
         //System.out.println("Strat: " + strategy1.fancyToString(rootState, expander, rootState.getAllPlayers()[1]));
     }
@@ -333,7 +385,8 @@ public class SMConvergenceExperiment {
     // game algorithm iterations_per_output
     //arguments: Anti[EL]D/GSX/RNDYYY OOS6/Exp3[MV][RK]2 100000
     
-    private static int iterations = 10000000;
+    private static int runs=50;
+    private static int iterations = (int) 1e6;
     private static String algorithm = "Exp3";
     private static boolean keepExploration = false;
     private static boolean propagateMeans = false;
@@ -352,10 +405,17 @@ public class SMConvergenceExperiment {
                 propagateMeans = args[1].charAt(4) == 'M';
                 keepExploration = args[1].charAt(5) == 'K';
                 break;
+            default:
+                assert args[1].startsWith("RM");
+                algorithm = "RM";
+                gamma = Double.parseDouble(args[1].substring(4))/10;
+                if (args[1].charAt(4)=='0') gamma /= 10;
+                propagateMeans = args[1].charAt(2) == 'M';
+                keepExploration = args[1].charAt(3) == 'K';
         }
         iterations=Integer.parseInt(args[2]);
         
-        for (int j=0;j<100;j++) {
+        for (int j=0;j<runs;j++) {
             switch(args[0].substring(0, 2)){
                 case "An":
                     boolean expRewards = args[0].charAt(4)=='E';
@@ -371,20 +431,23 @@ public class SMConvergenceExperiment {
             if (algorithm.equals("OOS")){
                 runMCTSOOS();
             } else {
-                runMCTSExp3();
+                if (args[0].substring(0, 2).equals("An")){
+                  runISMCTS();
+                } else runSMMCTS_Decoupled();
             }
             
         }
     }
     
     public static void main(String[] args) throws Exception {
-        //batchMain(args);
+        batchMain(args);
         //setupGoofSpiel(3);
         //setupOshiZumo(8, 2);
-        setupRnd(6);
+        //setupRnd(6);
         //setupAntiExploration();
-        gamma=0.001;
-        runSMMCTS_Exp3();
+//        setupAntiMCTS(5, true);
+//        gamma=0.01;
+//        runSMMCTS_Exp3();
         //runMCTSExp3();
     }
 }
