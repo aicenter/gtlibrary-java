e2c770a9a2a9f9dd41b26f2bfac689da3552b716
Jiri Cermak <jiri.cermak@agents.fel.cvut.cz>
2014-11-11 09:26:09 +0100

    Fixed Objective MILPs if some sequences are missing

--- src/cz/agents/gtlibrary/algorithms/sequenceform/gensum/BestGenSumSequenceFormMILP.java
+++ src/cz/agents/gtlibrary/algorithms/sequenceform/gensum/BestGenSumSequenceFormMILP.java
@@ -6,7 +6,7 @@ import cz.agents.gtlibrary.interfaces.Sequence;
 
 import java.util.Map;
 
-public class BestGenSumSequenceFormMILP extends BRGenSumSequenceFormMILP{
+public class BestGenSumSequenceFormMILP extends BRGenSumSequenceFormMILP {
 
     public BestGenSumSequenceFormMILP(GenSumSequenceFormConfig config, Player[] players, GameInfo info, Player player, Map<Sequence, Double> opponentRealPlan) {
         super(config, players, info, player, opponentRealPlan);
@@ -16,8 +16,10 @@ public class BestGenSumSequenceFormMILP extends BRGenSumSequenceFormMILP{
         for (Map.Entry<Map<Player, Sequence>, Double[]> entry : config.getUtilityForSequenceCombinationGenSum().entrySet()) {
             Sequence playerSequence = entry.getKey().get(player);
             double utility = entry.getValue()[player.getId()];
+            Double opponentProbability = opponentRealPlan.get(entry.getKey().get(info.getOpponent(player)));
 
-            lpTable.addToObjective(playerSequence, utility * opponentRealPlan.get(entry.getKey().get(info.getOpponent(player))));
+            if (opponentProbability != null)
+                lpTable.addToObjective(playerSequence, utility * opponentProbability);
         }
     }
 }
