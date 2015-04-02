4b2ff8208179c3e1d2899096b37f080bfbe348da
Jiri Cermak <jiri.cermak@agents.fel.cvut.cz>
2015-04-02 17:49:32 +0200

    Dominance now uses List<Action> as a result instead of List<ActionPureStrategy>

--- src/cz/agents/gtlibrary/utils/SimGameStrategyDominance.java
+++ src/cz/agents/gtlibrary/utils/SimGameStrategyDominance.java
@@ -35,8 +35,8 @@ public class SimGameStrategyDominance {
 
     public static void main(String[] args) {
 //        Map<Player, StrategyDominance.DominanceResult> result = runOshiZumo();
-//        Map<Player, StrategyDominance.DominanceResult> result = runSimRnd();
-        Map<Player, StrategyDominance.DominanceResult> result = runGoofspiel();
+        Map<Player, StrategyDominance.DominanceResult> result = runSimRnd();
+//        Map<Player, StrategyDominance.DominanceResult> result = runGoofspiel();
 
         System.out.println(result);
     }
@@ -69,30 +69,30 @@ public class SimGameStrategyDominance {
                 new NatureCacheImpl(),
                 new LowerBoundComparatorFactory());
         CompleteUtilityCalculator calculator = new CompleteUtilityCalculator(data);
-        List<ActionPureStrategy> strategies1 = getActionStrategies(rootState, expander);
-        List<ActionPureStrategy> strategies2 = getActionStrategies(rootState.performAction(strategies1.get(0).getAction()), expander);
-        Map<ActionPureStrategy, double[]> p1Utilities = new HashMap<>(strategies1.size());
-        Map<ActionPureStrategy, double[]> p2Utilities = new HashMap<>(strategies2.size());
+        List<Action> strategies1 = expander.getActions(rootState);
+        List<Action> strategies2 = expander.getActions(rootState.performAction(strategies1.get(0)));
+        Map<Action, double[]> p1Utilities = new HashMap<>(strategies1.size());
+        Map<Action, double[]> p2Utilities = new HashMap<>(strategies2.size());
 
-        for (ActionPureStrategy strategy : strategies1) {
-            p1Utilities.put(strategy, new double[strategies2.size()]);
+        for (Action action : strategies1) {
+            p1Utilities.put(action, new double[strategies2.size()]);
         }
-        for (ActionPureStrategy strategy : strategies2) {
-            p2Utilities.put(strategy, new double[strategies1.size()]);
+        for (Action action : strategies2) {
+            p2Utilities.put(action, new double[strategies1.size()]);
         }
         int strategy1Idx = 0;
         int strategy2Idx = 0;
 
-        for (ActionPureStrategy strategy1 : strategies1) {
-            for (ActionPureStrategy strategy2 : strategies2) {
-                double value = calculator.getUtility(rootState.performAction(strategy1.getAction()).performAction(strategy2.getAction()), strategy1, strategy2);
-                p1Utilities.get(strategy1)[strategy2Idx++] = value;
-                p2Utilities.get(strategy2)[strategy1Idx] = -value;
+        for (Action action1 : strategies1) {
+            for (Action action2 : strategies2) {
+                double value = calculator.getUtility(rootState.performAction(action1).performAction(action2), new ActionPureStrategy(action1), new ActionPureStrategy(action2));
+                p1Utilities.get(action1)[strategy2Idx++] = value;
+                p2Utilities.get(action2)[strategy1Idx] = -value;
             }
             strategy1Idx++;
             strategy2Idx = 0;
         }
-        StrategyDominance<ActionPureStrategy> dominance = new StrategyDominance<>();
+        StrategyDominance<Action> dominance = new StrategyDominance<>();
         Map<Player, StrategyDominance.DominanceResult> result = new HashMap<>(2);
 
         result.put(gameInfo.getAllPlayers()[0], dominance.computeMixedDominance(p1Utilities));
