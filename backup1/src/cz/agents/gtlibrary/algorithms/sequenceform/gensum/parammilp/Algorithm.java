751b24247f7537dc3e6aeb257938f97d3f02717a
jiricermak <jiricermak@jiricermak-ntb>
2015-10-19 17:14:47 +0200

    cleanup

--- backup1/src/cz/agents/gtlibrary/algorithms/sequenceform/gensum/parammilp/Algorithm.java
+++
@@ -1,7 +0,0 @@
-package cz.agents.gtlibrary.algorithms.sequenceform.gensum.parammilp;
-
-import cz.agents.gtlibrary.algorithms.sequenceform.gensum.SolverResult;
-
-public interface Algorithm {
-    public SolverResult compute();
-}
