61aa767cef712fc9d52d2d6ab0598b96313a92d8
jiricermak <jiricermak@jiricermak-ntb>
2015-10-19 17:04:25 +0200

    Final version of AAAI experiments

--- src/cz/agents/gtlibrary/algorithms/stackelberg/iterativelp/bfs/CompleteBFSEnforcingStackelbergLP.java
+++ src/cz/agents/gtlibrary/algorithms/stackelberg/iterativelp/bfs/CompleteBFSEnforcingStackelbergLP.java
@@ -6,6 +6,7 @@ import cz.agents.gtlibrary.interfaces.InformationSet;
 import cz.agents.gtlibrary.interfaces.Player;
 import cz.agents.gtlibrary.interfaces.Sequence;
 import cz.agents.gtlibrary.utils.Pair;
+import cz.agents.gtlibrary.utils.Triplet;
 import ilog.concert.IloException;
 import ilog.cplex.IloCplex;
 
@@ -13,17 +14,17 @@ import java.util.*;
 
 public class CompleteBFSEnforcingStackelbergLP extends BFSEnforcingStackelbergLP {
 
-    private Queue<Pair<Set<Sequence>, Double>> queue;
+    private Queue<Triplet<Set<Sequence>, Iterable<Sequence>, Double>> queue;
     private Set<Sequence> currentRestriction;
     private double lowerBound;
     private Pair<Map<Sequence, Double>, Double> currentBest = dummyResult;
 
     public CompleteBFSEnforcingStackelbergLP(Player leader, GameInfo info) {
         super(leader, info);
-        queue = new PriorityQueue<>(new Comparator<Pair<Set<Sequence>, Double>>() {
+        queue = new PriorityQueue<>(new Comparator<Triplet<Set<Sequence>, Iterable<Sequence>, Double>>() {
             @Override
-            public int compare(Pair<Set<Sequence>, Double> o1, Pair<Set<Sequence>, Double> o2) {
-                return Double.compare(o2.getRight(), o1.getRight());
+            public int compare(Triplet<Set<Sequence>, Iterable<Sequence>, Double> o1, Triplet<Set<Sequence>, Iterable<Sequence>, Double> o2) {
+                return Double.compare(o2.getThird(), o1.getThird());
             }
         });
         lowerBound = -info.getMaxUtility();
@@ -37,14 +38,19 @@ public class CompleteBFSEnforcingStackelbergLP extends BFSEnforcingStackelbergLP
         if(currentBest.getRight() > Double.NEGATIVE_INFINITY)
             return currentBest;
         while (!queue.isEmpty()) {
-            Pair<Set<Sequence>, Double> current = queue.poll();
+            Triplet<Set<Sequence>, Iterable<Sequence>, Double> current = queue.poll();
 
-            if (current.getRight() < lowerBound + eps)
+            if (current.getThird() < lowerBound + eps)
                 break;
 //            System.out.println(current.getLeft());
 //            System.out.println("UB: " + current.getRight());
-            updateRestrictions(lpData, current.getLeft());
-            lpData = solveFixedLP(upperBound);
+            for (Sequence sequence : current.getSecond()) {
+                Set<Sequence> currentFix = new HashSet<>(current.getFirst());
+
+                currentFix.add(sequence);
+                updateRestrictions(lpData, currentFix);
+                lpData = solveFixedLP(upperBound);
+            }
         }
         return currentBest;
     }
@@ -135,32 +141,28 @@ public class CompleteBFSEnforcingStackelbergLP extends BFSEnforcingStackelbergLP
 
     @Override
     protected Pair<Map<Sequence, Double>, Double> handleBrokenStrategyCause(double lowerBound1, double upperBound, LPData lpData, double value, Iterable<Sequence> brokenStrategyCauses) {
-        for (Sequence brokenStrategyCause : brokenStrategyCauses) {
-            restrictFollowerPlay(brokenStrategyCause, brokenStrategyCauses, lpData);
-//            System.out.println(currentRestriction + " " + brokenStrategyCause);
-            Pair<Iterable<Sequence>, Double> result = probe();
-
-            if (result.getRight() > Double.NEGATIVE_INFINITY) {
-                if (result.getLeft() == null) {
-                    if (lowerBound <= result.getRight()) {
-                        lowerBound = result.getRight();
-                        if (result.getRight() > currentBest.getRight())
-                            currentBest = new Pair<>(null, result.getRight());
-                    }
-                } else {
-                    if (result.getRight() > lowerBound + eps) {
-                        for (Sequence sequence : result.getLeft()) {
-                            Set<Sequence> copy = new HashSet<>(currentRestriction);
-
-                            copy.add(brokenStrategyCause);
-                            copy.add(sequence);
-                            queue.add(new Pair<>(copy, result.getRight()));
-                        }
-                    }
-                }
-            }
-            removeRestriction(brokenStrategyCause, brokenStrategyCauses, lpData);
-        }
-        return currentBest;
+        queue.add(new Triplet<Set<Sequence>, Iterable<Sequence>, Double>(new HashSet<>(currentRestriction), brokenStrategyCauses, value));
+        return null;
+//        for (Sequence brokenStrategyCause : brokenStrategyCauses) {
+//            restrictFollowerPlay(brokenStrategyCause, brokenStrategyCauses, lpData);
+////            System.out.println(currentRestriction + " " + brokenStrategyCause);
+//            Pair<Iterable<Sequence>, Double> result = probe();
+//
+//            if (result.getRight() > Double.NEGATIVE_INFINITY) {
+//                if (result.getLeft() == null) {
+//                    if (lowerBound <= result.getRight()) {
+//                        lowerBound = result.getRight();
+//                        if (result.getRight() > currentBest.getRight())
+//                            currentBest = new Pair<>(null, result.getRight());
+//                    }
+//                } else {
+//                    if (result.getRight() > lowerBound + eps) {
+//                        queue.add(new Triplet<Set<Sequence>, Iterable<Sequence>, Double>(new HashSet<>(currentRestriction), result.getLeft(), result.getRight()));
+//                    }
+//                }
+//            }
+//            removeRestriction(brokenStrategyCause, brokenStrategyCauses, lpData);
+//        }
+//        return currentBest;
     }
 }
--- src/cz/agents/gtlibrary/domain/bpg/BPGGameInfo.java
+++ src/cz/agents/gtlibrary/domain/bpg/BPGGameInfo.java
@@ -34,7 +34,7 @@ public class BPGGameInfo implements GameInfo {
 	public static boolean SLOW_MOVES = true;
 	public static String graphFile = "GridW3-almost-connected.txt";//"GridW4.txt";"GridW3-small.txt"
     public static double EVADER_MOVE_COST = 0.5/(2*DEPTH);
-    public static final double DEFENDER_MOVE_COST = 0.5/(2*DEPTH);
+    public static double DEFENDER_MOVE_COST = 0.5/(2*DEPTH);
 
 	@Override
 	public double getMaxUtility() {
