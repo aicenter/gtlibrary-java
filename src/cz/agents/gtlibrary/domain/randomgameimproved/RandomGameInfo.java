2756c0f5bb398084698e461d4b40d4a69036204e
Branislav Bosansky <branislav.bosansky@agents.fel.cvut.cz>
2016-02-16 15:18:10 -0700

    IRMM v0.11 -- fisrt version of BNB over LP fixed

--- src/cz/agents/gtlibrary/domain/randomgameimproved/RandomGameInfo.java
+++ src/cz/agents/gtlibrary/domain/randomgameimproved/RandomGameInfo.java
@@ -22,7 +22,7 @@ public class RandomGameInfo implements GameInfo {
     public static final Player[] ALL_PLAYERS = new Player[]{FIRST_PLAYER, SECOND_PLAYER, NATURE};
 
     public static double CORRELATION = -1;// -1 for zero sum, 1 for identical utilities
-    public static int MAX_DEPTH = 5;
+    public static int MAX_DEPTH = 6;
     public static int MAX_BF = 2;
     public static int MAX_OBSERVATION = 2;
     public static int MAX_UTILITY = 1;
