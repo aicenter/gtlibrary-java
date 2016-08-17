661fd99737e35032ce259cf7c807d9134507e8b3
Jiri Cermak <jiri.cermak@agents.fel.cvut.cz>
2016-08-17 14:01:21 +0200

    All the candidates are now resolved if their bounds come from smaller restricted game

---
+++ src/cz/agents/gtlibrary/algorithms/bestresponse/ImperfectRecallBestResponse.java
@@ -0,0 +1,12 @@
+package cz.agents.gtlibrary.algorithms.bestresponse;
+
+import cz.agents.gtlibrary.interfaces.Action;
+import cz.agents.gtlibrary.interfaces.Sequence;
+
+import java.util.Map;
+
+public interface ImperfectRecallBestResponse {
+    public Map<Action, Double> getBestResponse(Map<Action, Double> opponentStrategy);
+    public Map<Action, Double> getBestResponseSequence(Map<Sequence, Double> opponentStrategy);
+    public double getValue();
+}
