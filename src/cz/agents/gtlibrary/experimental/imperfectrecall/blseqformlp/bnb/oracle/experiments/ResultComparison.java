11c170820769af2bcb7642750ae1432c3a82f84a
jiricermak <jiricermak@jiricermak-ntb>
2016-09-01 16:56:59 +0200

    Duplicate change generation removed

--- src/cz/agents/gtlibrary/experimental/imperfectrecall/blseqformlp/bnb/oracle/experiments/ResultComparison.java
+++ src/cz/agents/gtlibrary/experimental/imperfectrecall/blseqformlp/bnb/oracle/experiments/ResultComparison.java
@@ -11,7 +11,7 @@ import java.lang.management.ThreadMXBean;
 public class ResultComparison {
 
     public static void main(String[] args) {
-        int startingSeed = (args.length > 0) ? new Integer(args[0]) : 14;
+        int startingSeed = (args.length > 0) ? new Integer(args[0]) : 1;
         int endingSeed = (args.length > 1) ? new Integer(args[1]) : 200;
 
         int BF = (args.length > 3) ? new Integer(args[3]) : RandomGameInfo.MAX_BF;
@@ -36,7 +36,7 @@ public class ResultComparison {
             double oracleBnBValue = OracleBilinearSequenceFormBnB.runRandomGame();
 
             System.out.println("seed: " + seed + ": " + milpValue + " vs " + oracleBnBValue/* + " vs " + milpValue*/);
-            if (Math.abs(oracleBnBValue - milpValue) > 0.5) {/* &&  < 1e-2*/
+            if (Math.abs(oracleBnBValue - milpValue) > 0.1) {/* &&  < 1e-2*/
                 throw new IllegalStateException();
             }
             System.out.println("-----------------------");
