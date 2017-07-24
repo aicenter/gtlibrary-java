895f11b0986e5ebc81a5caea248a7aa56f082ec6
Jiri Cermak <jiri.cermak@agents.fel.cvut.cz>
2017-07-24 16:16:30 +0200

    Iterations now initiated properly, Wichardt domain can now be used for testing

--- src/cz/agents/gtlibrary/experimental/imperfectrecall/automatedabstractions/IRFictitiousPlay.java
+++ src/cz/agents/gtlibrary/experimental/imperfectrecall/automatedabstractions/IRFictitiousPlay.java
@@ -35,7 +35,7 @@ public class IRFictitiousPlay extends ALossPRCFRBR {
 
     public static void main(String[] args) {
 //        runGenericPoker();
-        runIRGenericPoker();
+//        runIRGenericPoker();
 //        runWichardtCounterExample();
 //        runBothIRRandomAbstractionGame();
 //        runCPRRBothIRRandomAbstractionGame();
